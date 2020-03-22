a309c854cdfbf4a54b67a2deed09bdca425180d6
Zhicheng Zhang <zz2555@columbia.edu>
2020-03-22 15:44:03 -0400

    Data Challenge

--- Part2/SQL Problem1.sql
+++ Part2/SQL Problem1.sql
@@ -1,6 +1,6 @@
 SELECT
 LEFT(u.created_at,7) AS Month
-,concat(ROUND(AVG(
+,CONCAT(ROUND(AVG(
 IF(LEFT(u.created_at,7)=LEFT(e.exercise_completion_date,7),1,0))*100,2),'%') AS Percentage
 FROM 
 exercises e
